6b7c87df91f55b4cd80aac26de10877b6080ddc5
Madhuri Tayade <codekul.madhuri@gmail.com>
2022-07-18 17:23:35 +0530

    MAdhuri - aop @before

--- src/main/java/com/codekul/Spring11JuneBatch/aop/StudentAop.java
+++ src/main/java/com/codekul/Spring11JuneBatch/aop/StudentAop.java
@@ -1,15 +1,27 @@
 package com.codekul.Spring11JuneBatch.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
 @Slf4j
 @Aspect
 @Component
 public class StudentAop {
     @Before( "execution(* com.codekul.Spring11JuneBatch.jpa.controller.StudentController.findAllStudent(..))")
-    public void findAllStudentAop(){
+    public void findAllStudentAopBefore(){
       log.info("Inside before find all students");
+    }
+
+    @After( "execution(* com.codekul.Spring11JuneBatch.jpa.controller.StudentController.findAllStudent(..))")
+    public void findAllStudentAopAfter(){
+        log.info("Inside after find all students");
+    }
+
+    @Before( "execution(* com.codekul.Spring11JuneBatch.jpa.controller.StudentController.findStudentByName(..)) && args(name)" )
+    public void findStudentByName(String name){
+        log.info("Executing find student by name method "+name);
     }
 }
